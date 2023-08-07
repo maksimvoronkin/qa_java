f6a695a8ae4d24c01ce45088db0b765f1f11b130
Maksim Voronkin <jh1337@yandex.ru>
2023-08-07 21:45:36 +0300

    remove .idea and add mockito

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,21 +2,27 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+    @Mock
+    Feline feline;
     @Test
     public void getSoundCatMayTest() {
-        Feline feline = new Feline();
         Cat cat = new Cat(feline);
         Assert.assertEquals("Мяу", cat.getSound());
     }
 
     @Test
     public void getFoodCatCorrectGoodTest() throws Exception {
-        Feline feline = new Feline();
         Cat cat = new Cat(feline);
-        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+        cat.getFood();
+        Mockito.verify(feline).eatMeat();
     }
 }
